d7fdd0277050da9318304097ba25a08260cb7c96
LeeChang_Mac <dlckd125@nate.com>
2024-11-26 00:25:22 +0900

    first commit

---
+++ src/main/java/com/example/kafkapush/KafkaPushApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkapush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkapush/KafkaPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkapush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
